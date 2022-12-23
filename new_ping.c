7a698105658794b4ffcb30b28953a31e37ee23bf
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 <royx520@gmail.com>
2022-12-23 13:03:02 +0200

    add file

--- new_ping.c
+++ new_ping.c
@@ -1,12 +1,50 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
 
-// run 2 programs using fork + exec
-// command: make clean && make all && ./partb
+#define PORT 9999
+#define SERVER_IP_ADDRESS "0.0.0.0"
+
+
 int main()
 {
-    /*char *args[2];
+    int sock = socket(AF_INET, SOCK_STREAM, 0); // create socket
+    if (sock == -1)
+    {
+        printf("Unable to create a socket : %d", errno);
+        exit(1);
+    }
+
+    //"sockaddr_in" used for IPv4 communication
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress)); // file struct with 0
+    serverAddress.sin_family = AF_INET; // work with ipv4
+    serverAddress.sin_port = htons(PORT); // insert to struct the port 
+    int rval = inet_pton(AF_INET, (const char *)SERVER_IP_ADDRESS, &serverAddress.sin_addr);//convert the address to binary
+    if (rval <= 0)
+    {
+        printf("inet_pton failed");
+        exit(1);
+    }
+
+    if (connect(sock, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) // Make a connection to the receiver with socket
+    {
+        printf("connect failed with error code : %d", errno);
+    }
+
+    printf("connected to server\n\n");
+
+
+
+    char *args[2];
     // compiled watchdog.c by makefile
     args[0] = "./watchdog";
     args[1] = NULL;
@@ -19,5 +57,10 @@ int main()
     }
     wait(&status); // waiting for child to finish before exiting
     printf("child exit status is: %d", status);
-    return 0;*/
+
+
+
+
+    
+    return 0;
 }
--- watchdog.c
+++ watchdog.c
@@ -1,14 +1,95 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h> 
+#include <sys/time.h>
+
+#define SERVER_PORT 3000
+struct timeval start, end;
 
 int main()
 {
-    /*printf("hello partb");
-    while (timer < 10seconds)
-    {
-        recv();
-        timer = 0seconds;
-    }
-    send("timeout");
+    int listenSocket = -1; // create listening socket
+	if ((listenSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Could not create listening socket : %d", errno);
+		exit(1);
+	}
+	printf("Sokcet created\n");
 
-        return 0;*/
+	int yes = 1; // check if the ip in not in use
+	if (setsockopt(listenSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
+	{
+		perror("setsockopt");
+		exit(1);
+	}
+
+	//"sockaddr_in" used for IPv4 communication
+	struct sockaddr_in serverAddress;
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_port = htons(SERVER_PORT);
+
+	// Bind the socket to the port with any IP at this port
+	if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+	{
+		printf("Bind failed with error code : %d", errno);
+		close(listenSocket);
+		exit(1);
+	}
+
+	printf("Bind success\n");
+
+	// Make the socket listening; actually mother of all client sockets.
+	if (listen(listenSocket, 1) == -1) // 1 is a Maximum size of queue connection requests
+	{
+		printf("listen failed with error code : %d", errno);
+		close(listenSocket);
+		exit(1);
+	}
+
+	printf("Waiting for incoming TCP-connections\n");
+
+	struct sockaddr_in ClientAddress;
+	socklen_t clientAddressLen = sizeof(ClientAddress);
+	memset(&ClientAddress, 0, sizeof(ClientAddress)); // file struct with 0
+
+	int ClientSocket = accept(listenSocket, (struct sockaddr *)&ClientAddress, &clientAddressLen); // create socket to sender
+	if (ClientSocket == -1)
+	{
+		printf("listen failed with error code : %d", errno);
+		return -1;
+	}
+
+	printf("A new client connection accepted\n\n");
+
+    while (1)
+    {
+        int BytesLeft = 5; // intialize how much byte left to received
+		char buffer[1024];
+		int BytesReceived = 0;// countig how much byte received from sender
+		while (BytesReceived < 5)
+		{
+			int MessRecv = recv(ClientSocket, buffer, BytesLeft, 0); // receive the message 
+			BytesReceived += MessRecv; // add the number of byte that arrive from sender
+			BytesLeft -= MessRecv; // subtraction the number of byte that left to receive
+		}
+        gettimeofday(&end,0); // stop measure time
+        double timer=(end.tv_sec - start.tv_sec)+(end.tv_usec-start.tv_usec)*1e-6;
+        gettimeofday(&start,0); // stop measure time
+        if (timer>10)
+        {
+            send("timeout");
+        }
+        
+    }
+    close(ClientSocket);
+    return 0;
 }
